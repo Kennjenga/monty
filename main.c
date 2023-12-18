4aa2995f1d0563cbbe5b19047a9f19908d366ef3
Kennjenga <108663105+Kennjenga@users.noreply.github.com>
2023-12-18 16:19:01 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,89 +1,31 @@
-#include <stdlib.h>
+
+
+#include "monty.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+char **op_toks = NULL;
 
 /**
- * get_int - gets a character pointer to new string containing int
- * @num: number to convert to string
+ * main - the entry point for Monty Interp
  *
- * Return: character pointer to newly created string. NULL if malloc fails.
- */
-char *get_int(int num)
-{
-	unsigned int temp;
-	int length = 0;
-	long num_l = 0;
-	char *ret;
-
-	temp = _abs(num);
-	length = get_numbase_len(temp, 10);
-
-	if (num < 0 || num_l < 0)
-		length++; /* negative sign */
-	ret = malloc(length + 1); /* create new string */
-	if (!ret)
-		return (NULL);
-
-	fill_numbase_buff(temp, 10, ret, length);
-	if (num < 0 || num_l < 0)
-		ret[0] = '-';
-
-	return (ret);
-}
-
-/**
- * _abs - gets the absolute value of an integer
- * @i: integer to get absolute value of
+ * @argc: the count of arguments passed to the program
+ * @argv: pointer to an array of char pointers to arguments
  *
- * Return: unsigned integer abs rep of i
+ * Return: (EXIT_SUCCESS) on success (EXIT_FAILURE) on error
  */
-unsigned int _abs(int i)
+int main(int argc, char **argv)
 {
-	if (i < 0)
-		return (-(unsigned int)i);
-	return ((unsigned int)i);
-}
+	FILE *script_fd = NULL;
+	int exit_code = EXIT_SUCCESS;
 
-/**
- * get_numbase_len - gets length of buffer needed for an unsigned int
- * @num: number to get length needed for
- * @base: base of number representation used by buffer
- *
- * Return: integer containing length of buffer needed (doesn't contain null bt)
- */
-int get_numbase_len(unsigned int num, unsigned int base)
-{
-	int len = 1; /* all numbers contain atleast one digit */
-
-	while (num > base - 1)
-	{
-		len++;
-		num /= base;
-	}
-	return (len);
-}
-
-/**
- * fill_numbase_buff - fills buffer with correct numbers up to base 36
- * @num: number to convert to string given base
- * @base: base of number used in conversion, only works up to base 36
- * @buff: buffer to fill with result of conversion
- * @buff_size: size of buffer in bytes
- *
- * Return: always void.
- */
-void fill_numbase_buff(unsigned int num, unsigned int base,
-			char *buff, int buff_size)
-{
-	int rem, i = buff_size - 1;
-
-	buff[buff_size] = '\0';
-	while (i >= 0)
-	{
-		rem = num % base;
-		if (rem > 9) /* return lowercase ascii val representation */
-			buff[i] = rem + 87; /* 10 will be a, 11 = b, ... */
-		else
-			buff[i] = rem + '0';
-		num /= base;
-		i--;
-	}
+	if (argc != 2)
+		return (usage_error());
+	script_fd = fopen(argv[1], "r");
+	if (script_fd == NULL)
+		return (f_open_error(argv[1]));
+	exit_code = run_monty(script_fd);
+	fclose(script_fd);
+	return (exit_code);
 }
